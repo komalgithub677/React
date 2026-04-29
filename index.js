a3c6b126c7c037df81c30c9baefa25401149fbf9
Komal Narawade <knarawade4@gmail.com>
2026-04-29 11:22:56 +0530

    Day 1

---
+++ index.js
@@ -0,0 +1,14 @@
+// console.log("hello day 4 js")
+
+// public void m1(){
+
+// }
+
+function greet (){
+        console.log("hello.. good morining all..")
+}
+
+//no need to create object here 
+//directly call the function 
+greet();
+
